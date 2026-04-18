4c9e83f1145bb5f9b2435c7ababb04f61e330ff2
Aprajita <mishraaprajita43@gmail.com>
2026-04-19 02:05:19 +0530

    DSA LDST

--- Stack/esay.py
+++ Stack/esay.py
@@ -2,27 +2,31 @@ class Stack:
     def __init__(self):
         self.stack = []
 
-    def push(self,item):
-        self.stack.append(item)
-        print(item,"append")  
+    def push(self, x):
+        self.stack.append(x)
 
     def pop(self):
-        if len(self.stack)==0:
-            print("stack underflow")      
-        else:
-            (self.stack.pop(),"element popped")    
+        if not self.stack:
+            print("Underflow")
+            return None
+        return self.stack.pop()
 
     def peek(self):
-        if len(self.stack)==0:
-            print("Stack is empty")
-        else:
-            print(self.stack[0])     
+        if not self.stack:
+            print("Underflow")
+            return None
+        return self.stack[-1] 
+
+
 
 s = Stack()
-s.push(10)       
-s.push(20)       
-s.push(30)  
 
-s.pop()
+s.push(10)
+s.push(20)
+s.push(30)
 
-s.display()
+print("Top:", s.peek())
+print("Pop:", s.pop())
+print("Pop:", s.pop())
+print("Pop:", s.pop())
+print("Pop:", s.pop())   
